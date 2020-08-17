71768069708c69dab31cfe81bb4d0f83a0ca19b7
prafful <prraful@gmail.com>
2020-08-17 00:39:36 -0700

    day 01 - react native basics!

--- App.js
+++ App.js
@@ -2,13 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default class App extends React.Component {
+  
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textstyle}>React Native! <Text>Style Inheritance </Text></Text>
+      <Text>React Native is easy.Open up App.js to start working on your app!</Text>
+      <Text>I can deploy for multiple devices!!!!</Text>
+      <Text>Tunnel Works!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +23,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor:'pink',
+    color:'blue'
   },
+  textstyle:{
+    color:'red',
+    fontSize:22
+  }
 });
